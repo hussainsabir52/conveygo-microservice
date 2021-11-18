8642c9677c2841bd26cbd2ea372e16e9aa3d862e
hussainsabir52 <hussucool0@gmail.com>
2021-11-18 19:49:26 +0500

    err

--- drivers/webserver/routes/user.js
+++ drivers/webserver/routes/user.js
@@ -24,7 +24,7 @@ users.login = async (req, res, next) => {
     userDB.userLogin(req.body).then((data) => {
         res.send(data);
     }).catch((err) => {
-        res.json(err);
+        return err;
     })
 };
 
